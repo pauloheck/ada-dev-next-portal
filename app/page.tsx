c4ede8b6d75dd9cd95376bb2a406e4d3f36ec32e
pauloheck <pauloheck@gmail.com>
2024-11-23 20:18:53 -0300

    style: Improve code formatting and indentation in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,36 @@ export default function Home() {
       <header className="bg-gray-900 text-white p-4 shadow-md">
         <nav className="container mx-auto flex justify-center">
           <ul className="flex space-x-8">
-            <li className="text-lg font-bold"><a href="/" className="hover:text-gray-400">AdaDev</a></li>
-            <li><a href="/story" className="hover:text-gray-400">Create Story</a></li>
+            <li className="text-lg font-bold">
+              <a href="/" className="hover:text-gray-400">
+                AdaDev
+              </a>
+            </li>
+            <li>
+              <a href="/story" className="hover:text-gray-400">
+                Create Story
+              </a>
+            </li>
           </ul>
         </nav>
       </header>
       <main className="container mx-auto p-8">
         <section id="mission" className="my-12">
           <h1 className="text-4xl font-bold mb-4">AdaDev</h1>
-          <h2 className="text-3xl font-semibold mb-4">Transformando o Futuro do Desenvolvimento de Software</h2>
-          <p className="text-lg">Capacitar desenvolvedores com IA avançada para otimizar o desenvolvimento de software.</p>
+          <h2 className="text-3xl font-semibold mb-4">
+            Transformando o Futuro do Desenvolvimento de Software
+          </h2>
+          <p className="text-lg">
+            Capacitar desenvolvedores com IA avançada para otimizar o
+            desenvolvimento de software.
+          </p>
         </section>
-        <section id="vision" className="my-12">
-          <h2 className="text-3xl font-bold mb-4">Visão</h2>
-          <p className="text-lg">Ser a plataforma líder em soluções de IA para desenvolvimento de software.</p>
-        </section>
+
         <section id="services" className="my-12">
           <h2 className="text-3xl font-bold mb-4">Inovação constante</h2>
-          <p className="text-lg">Transforme Suas Ideias em Realidade com AdaDev!</p>
+          <p className="text-lg">
+            Transforme Suas Ideias em Realidade com AdaDev!
+          </p>
         </section>
       </main>
       <footer className="bg-gray-900 text-white p-4 text-center">
